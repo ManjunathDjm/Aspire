e3c2c53f7fdbcaf45bd49fa3316f6efb1692d5e5
Manjunath D J <manjunath@onco.com>
2019-08-09 13:27:30 +0530

    removed part of code in extent reports

--- src/com/onco/testbase/BaseClass.java
+++ src/com/onco/testbase/BaseClass.java
@@ -1,16 +1,11 @@
 package com.onco.testbase;
 
-import java.io.File;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
-import org.apache.commons.io.FileUtils;
+
 import org.apache.log4j.PropertyConfigurator;
-import org.apache.poi.hpsf.Date;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebDriverException;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -18,15 +13,12 @@ import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Parameters;
 
+import org.testng.annotations.Parameters;
 import com.onco.pages.Login;
 import com.onco.pages.SignUp;
 import com.onco.util.PropertiesData;
-import com.relevantcodes.extentreports.ExtentReports;
-import com.relevantcodes.extentreports.ExtentTest;
-import com.relevantcodes.extentreports.LogStatus;
+
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -39,37 +31,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class BaseClass {
 	  
 	public static WebDriver driver;
-	public static ExtentReports extent;
-	public static ExtentTest extentTest;
 	Properties prop=new Properties();
-	
-	
-	@BeforeTest
-	public void setExtent(){
-		extent = new ExtentReports(System.getProperty("user.dir")+"/test-output/ExtentReport.html", true);
-		extent.addSystemInfo("Host Name", "Manjunath Mac");
-		extent.addSystemInfo("User Name", "Onco");
-		extent.addSystemInfo("Environment", "QA");
-		
-	}
-	public void endReport(){
-		extent.flush();
-		extent.close();
-	}
-	
-	public static String getScreenshot(WebDriver driver, String screenshotName) throws IOException{
-		String dateName = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
-		TakesScreenshot ts = (TakesScreenshot) driver;
-		File source = ts.getScreenshotAs(OutputType.FILE);
-		// after execution, you could see a folder "FailedTestsScreenshots"
-		// under src folder
-		String destination = System.getProperty("user.dir") + "/FailedTestsScreenshots/" + screenshotName + dateName
-				+ ".png";
-		File finalDestination = new File(destination);
-		FileUtils.copyFile(source, finalDestination);
-		return destination;
-	}
-
 	
 	@Parameters({"browser","url"})
 	@BeforeMethod
@@ -125,23 +87,6 @@ public class BaseClass {
 	
 	   @AfterMethod
 	   public static void quit(ITestResult result) throws IOException {    
-		   if(result.getStatus()==ITestResult.FAILURE){
-				extentTest.log(LogStatus.FAIL, "TEST CASE FAILED IS "+result.getName()); //to add name in extent report
-				extentTest.log(LogStatus.FAIL, "TEST CASE FAILED IS "+result.getThrowable()); //to add error/exception in extent report
-				
-				String screenshotPath = BaseClass.getScreenshot(driver, result.getName());
-				extentTest.log(LogStatus.FAIL, extentTest.addScreenCapture(screenshotPath)); //to add screenshot in extent report
-				//extentTest.log(LogStatus.FAIL, extentTest.addScreencast(screenshotPath)); //to add screencast/video in extent report
-			}
-			else if(result.getStatus()==ITestResult.SKIP){
-				extentTest.log(LogStatus.SKIP, "Test Case SKIPPED IS " + result.getName());
-			}
-			else if(result.getStatus()==ITestResult.SUCCESS){
-				extentTest.log(LogStatus.PASS, "Test Case PASSED IS " + result.getName());
-
-			}
-			
-			extent.endTest(extentTest); //ending test and ends the current test and prepare to create html report
 			driver.quit();
 	
 	      
