d1bcd591ce64a272fd2be3954bf68ff1874cc45d
Manjunath D J <manjunath@onco.com>
2019-08-09 14:58:46 +0530

    added web driver wait in baseclass

--- src/com/onco/testbase/BaseClass.java
+++ src/com/onco/testbase/BaseClass.java
@@ -18,6 +18,7 @@ import org.testng.annotations.Parameters;
 import com.onco.pages.Login;
 import com.onco.pages.SignUp;
 import com.onco.util.PropertiesData;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -46,6 +47,7 @@ public class BaseClass {
             	
             	WebDriverManager.chromedriver().setup(); 
             	driver = new ChromeDriver();
+            	WebDriverWait wait = new WebDriverWait(driver,30);
             	driver.get(url);
             	driver.manage().deleteAllCookies();
                 driver.manage().window().maximize();
--- src/com/onco/testbase/TestUtil.java
+++ src/com/onco/testbase/TestUtil.java
@@ -1,12 +1,15 @@
 package com.onco.testbase;
 
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 import com.onco.testbase.BaseClass;
 
 public class TestUtil extends BaseClass{
 
 	public static long PAGE_LOAD_TIMEOUT = 60;
 	public static long IMPLICIT_WAIT = 20;
+	WebDriverWait wait = new WebDriverWait(driver,30);
 	static JavascriptExecutor js;
 
 }
