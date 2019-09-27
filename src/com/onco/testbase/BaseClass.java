00e099ee217433c117964566c2be17c731b5812f
Manjunath D J <manjunath@onco.com>
2019-09-27 21:15:56 +0530

    uncommented log4j

--- src/com/onco/testbase/BaseClass.java
+++ src/com/onco/testbase/BaseClass.java
@@ -33,8 +33,8 @@ public class BaseClass {
 	@BeforeMethod
 	public void openBrowser(String browser, String url) throws Exception {
        
-    	//String log4jConfPath = "/Users/manjunathdj/Applications/OncoWeb/src/resources/log4j.properties";
-    	//PropertyConfigurator.configure(log4jConfPath);
+    	String log4jConfPath = "/Users/manjunathdj/Applications/OncoWeb/src/resources/log4j.properties";
+    	PropertyConfigurator.configure(log4jConfPath);
  
 		try {
             if (browser.equalsIgnoreCase("chrome")) {
