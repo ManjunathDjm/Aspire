218e1cc0642b9337f40d340cf662324b566fe226
Manjunath D J <manjunath@onco.com>
2019-09-30 18:01:52 +0530

    code enhancements for URl

--- src/com/onco/testbase/BaseClass.java
+++ src/com/onco/testbase/BaseClass.java
@@ -55,9 +55,9 @@ public class BaseClass {
             	options.addArguments("--disable-setuid-sandbox");
             	options.setExperimentalOption("useAutomationExtension", false);
             	driver = new ChromeDriver(options);
+            	driver.get(url);
             	driver.manage().deleteAllCookies();
                 driver.manage().window().maximize();
-            	driver.get(url);
                 driver.manage().timeouts().pageLoadTimeout(TestUtil.PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS);
         		driver.manage().timeouts().implicitlyWait(TestUtil.IMPLICIT_WAIT, TimeUnit.SECONDS);   
         		
