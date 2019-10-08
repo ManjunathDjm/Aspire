988b1432f8d67a52a8aeeae87f52e855fb9abf14
Manjunath D J <manjunath@onco.com>
2019-10-08 19:15:08 +0530

    commented browser capabalities to debug jenkins

--- src/com/onco/testbase/BaseClass.java
+++ src/com/onco/testbase/BaseClass.java
@@ -49,11 +49,16 @@ public class BaseClass {
             	
             	WebDriverManager.chromedriver().setup(); 
             	ChromeOptions options = new ChromeOptions();
+            	
+            	options.addArguments("window-size=1280,800");
             	options.addArguments("--no-sandbox");
-            	options.addArguments("--headless", "--disable-gpu", "--window-size=1382,774","--ignore-certificate-errors");
-            	options.addArguments("--disable-dev-shm-usage");
-            	options.addArguments("--disable-setuid-sandbox");
+            	options.addArguments("--headless");
             	options.setExperimentalOption("useAutomationExtension", false);
+            	//options.addArguments("--no-sandbox");
+            	//options.addArguments("--headless", "--disable-gpu", "--window-size=1382,774","--ignore-certificate-errors");
+            	//options.addArguments("--disable-dev-shm-usage");
+            	//options.addArguments("--disable-setuid-sandbox");
+            	//options.setExperimentalOption("useAutomationExtension", false);
             	
             	driver = new ChromeDriver(options);
             	driver.get(url);
