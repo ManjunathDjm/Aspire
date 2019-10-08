58b5b53476b666fc66231c34b54ca7cbe802eb7a
Manjunath D J <manjunath@onco.com>
2019-10-08 18:22:09 +0530

    changed browser dimension in base class

--- src/com/onco/webtests/NewUserOpinionIndianPanel.java
+++ src/com/onco/webtests/NewUserOpinionIndianPanel.java
@@ -68,6 +68,7 @@ login.otpDetails(otp);
 PatientInfo info=new PatientInfo();
 info.patientInfo(patientname);
 	
+Thread.sleep(30000);
 InitialAssessment assessment= new InitialAssessment();
 assessment.initialAssessmentClose();
 	
