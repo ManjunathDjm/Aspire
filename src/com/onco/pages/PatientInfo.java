5f280e235da3f62e9c9e92d302babdb8b91e6d9e
Manjunath D J <manjunath@onco.com>
2019-11-14 14:11:34 +0530

    changed xpath

--- src/com/onco/pages/PatientInfo.java
+++ src/com/onco/pages/PatientInfo.java
@@ -32,7 +32,7 @@ public class PatientInfo extends BaseClass{
 	
 	private static final Logger logger = Logger.getLogger(PatientInfo.class);
 
-	@FindBy(xpath="//input[@placeholder='Enter patient name (optional)']")
+	@FindBy(xpath="//div[@class='form-group row ng-star-inserted']//div[@class='col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6']")
 	WebElement patientName;
 	
 	@FindBy(name ="cancerType")
