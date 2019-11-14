0e66e0da705afad7d8f37ecdfe0d7cb681296e0e
Manjunath D J <manjunath@onco.com>
2019-11-14 13:16:09 +0530

    added chrome dependancy in pom

--- src/com/onco/testbase/TestUtil.java
+++ src/com/onco/testbase/TestUtil.java
@@ -5,8 +5,8 @@ import com.onco.testbase.BaseClass;
 
 public class TestUtil extends BaseClass{
 
-	public static long PAGE_LOAD_TIMEOUT = 2000;
-	public static long IMPLICIT_WAIT = 10;
+	public static long PAGE_LOAD_TIMEOUT = 100;
+	public static long IMPLICIT_WAIT = 30;
 	static JavascriptExecutor js;
 
 }
