94a6769aa37947014734b61223a2ef2048f60ba2
Manjunath D J <manjunath@onco.com>
2019-11-22 12:37:38 +0530

    change in pom xml

--- src/com/onco/testbase/TestUtil.java
+++ src/com/onco/testbase/TestUtil.java
@@ -1,11 +1,10 @@
 package com.onco.testbase;
 
-import org.openqa.selenium.JavascriptExecutor;
 import com.onco.testbase.BaseClass;
 
 public class TestUtil extends BaseClass{
 
 	public static long PAGE_LOAD_TIMEOUT = 80;
-	public static long IMPLICIT_WAIT = 120;
+	public static long IMPLICIT_WAIT = 30;
 
 }
