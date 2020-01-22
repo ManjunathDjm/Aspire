19a2cda4434f1efc6a4c6d328e68833bb7ef1229
Manjunath DJ <manjunathdj@Manjunaths-MacBook-Pro.local>
2020-01-22 18:56:01 +0530

    removed clear method for OPT

--- src/com/onco/pages/PatientInfo.java
+++ src/com/onco/pages/PatientInfo.java
@@ -38,7 +38,7 @@ public class PatientInfo extends BaseClass{
 	
 	private static final Logger logger = Logger.getLogger(PatientInfo.class);
 
-	@FindBy(xpath="/html[1]/body[1]/app-root[1]/patient-info[1]/div[1]/div[2]/div[2]/div[1]/div[1]/div[1]/div[3]/div[1]/div[2]/input[1]")
+	@FindBy(xpath="//*[@name='patientName']")
 	private WebElement patientname;
 	
 	@FindBy(name ="cancerType")
