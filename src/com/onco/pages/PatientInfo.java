cb1e6fc6aa83b78038bfb8684c46e9ebbc88012b
Manjunath DJ <manjunathdj@Manjunaths-MacBook-Pro.local>
2020-01-23 15:32:35 +0530

    change in pom structure

--- src/com/onco/pages/PatientInfo.java
+++ src/com/onco/pages/PatientInfo.java
@@ -38,7 +38,7 @@ public class PatientInfo extends BaseClass{
 	
 	private static final Logger logger = Logger.getLogger(PatientInfo.class);
 
-	@FindBy(name="/html/body/app-root/patient-info/div/div[2]/div[2]/div/div[1]/div/div[3]/div[1]/div[2]/input")
+	@FindBy(name="patientName")
 	private WebElement patientname;
 	
 	@FindBy(name ="cancerType")
