594a47fb12486c817ac70290893ee78bb80b8512
Manjunath DJ <manjunathdj@Manjunaths-MacBook-Pro.local>
2020-01-27 17:16:46 +0530

    deleted unwanted imports and modified loggers

--- src/com/onco/pages/Service.java
+++ src/com/onco/pages/Service.java
@@ -1,20 +1,14 @@
 package com.onco.pages;
 
-
-import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-import org.testng.Assert;
 
 import com.onco.testbase.BaseClass;
 import org.apache.log4j.Logger;
-import org.apache.regexp.REDebugCompiler;
 
 /**
  * Locators and methods for Service page
