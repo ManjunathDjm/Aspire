faa5f0fadbad287eec7c42e7c275c4a61b99aa0f
Manjunath DJ <manjunathdj@Manjunaths-MacBook-Pro.local>
2020-01-31 15:56:30 +0530

    change in locator for Patientname

--- src/com/onco/testbase/BaseClass.java
+++ src/com/onco/testbase/BaseClass.java
@@ -55,8 +55,8 @@ public class BaseClass {
             	driver.navigate().to(url);
             	
             	//driver.manage().deleteAllCookies();
-            	driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
-                driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+            	driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+                driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         		
         		
             }    	
