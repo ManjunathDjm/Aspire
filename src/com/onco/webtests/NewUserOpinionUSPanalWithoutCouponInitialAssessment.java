775b6ea43d5d0459b589093c01cecb071015f6d1
Manjunath D J <manjunath@onco.com>
2020-02-11 15:57:58 +0530

    organized code

--- src/com/onco/webtests/NewUserOpinionUSPanalWithoutCouponInitialAssessment.java
+++ src/com/onco/webtests/NewUserOpinionUSPanalWithoutCouponInitialAssessment.java
@@ -19,81 +19,81 @@ import com.onco.pages.Summary;
 import com.onco.testbase.BaseClass;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
-
 public class NewUserOpinionUSPanalWithoutCouponInitialAssessment extends BaseClass {
-	
-@BeforeClass
-@Parameters({"deleteURL","url","browser","phone","otp"})
 
-public void delete(String deleteURL,String url,String browser,String phone,String otp) throws Exception {		
-	
-WebDriverManager.chromedriver().setup();
+	@BeforeClass
+	@Parameters({ "deleteURL", "url", "browser", "phone", "otp" })
+
+	public void delete(String deleteURL, String url, String browser, String phone, String otp) throws Exception {
+
+		WebDriverManager.chromedriver().setup();
 		ChromeOptions options = new ChromeOptions();
-		options.addArguments("headless","start-maximized");
-options.addArguments("window-size=1920,1080");
-options.addArguments("no-sandbox"); ; 
-driver = new ChromeDriver(options);
-driver.get(url);
-driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
-driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-			
-SignUp sign=new SignUp(driver);
-sign.signupicon();
+		options.addArguments("headless", "start-maximized");
+		options.addArguments("window-size=1920,1080");
+		options.addArguments("no-sandbox");
+		driver = new ChromeDriver(options);
+		driver.get(url);
+		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
 
-Thread.sleep(10000);
-Login login= new Login(driver);
-login.login(phone);
+		SignUp sign = new SignUp(driver);
+		sign.signupicon();
 
-Thread.sleep(10000);
-login.otpDetails(otp);
-Thread.sleep(5000);
-driver.get(deleteURL);
-Thread.sleep(5000);
-driver.quit();
-					
-}		
-	
-@Parameters({"name","email","phone","otp","patientname","INDcoupon"})
-@Test(priority = 1)
-public void TC_01_HomePage(String name, String email, String phone,String otp,String patientname,String INDcoupon) throws InterruptedException {
-	
-Homepage home= new Homepage(driver);
-home.formfill(name ,email,phone);
-home.patientquery();
-home.submit();
+		Thread.sleep(10000);
+		Login login = new Login(driver);
+		login.login(phone);
 
-Thread.sleep(10000);
-Login login= new Login(driver);
-login.otpDetails(otp);
+		Thread.sleep(10000);
+		login.otpDetails(otp);
+		Thread.sleep(5000);
+		driver.get(deleteURL);
+		Thread.sleep(5000);
+		driver.quit();
 
-Thread.sleep(5000);
-PatientInfo info=new PatientInfo(driver);
-info.patientname(patientname);
-info.cancertype();
-info.cancerstage();
-info.condition();
-info.continuebutton();
+	}
 
-InitialAssessment assessment= new InitialAssessment(driver);
-assessment.ViewAssessment();
+	@Parameters({ "name", "email", "phone", "otp", "patientname", "INDcoupon" })
+	@Test(priority = 1)
+	public void TC_01_HomePage(String name, String email, String phone, String otp, String patientname,
+			String INDcoupon) throws InterruptedException {
 
-Service service=new Service(driver);
-service.opinion();
+		Homepage home = new Homepage(driver);
+		home.formfill(name, email, phone);
+		home.patientquery();
+		home.submit();
 
-OpinionType opinion =new OpinionType(driver);
-opinion.USPanalButton();
+		Thread.sleep(10000);
+		Login login = new Login(driver);
+		login.otpDetails(otp);
 
-Thread.sleep(10000);
-Summary summary= new Summary(driver);
-summary.deletecouponcode();
-summary.summary();
+		Thread.sleep(5000);
+		PatientInfo info = new PatientInfo(driver);
+		info.patientname(patientname);
+		info.cancertype();
+		info.cancerstage();
+		info.condition();
+		info.continuebutton();
 
-PaymentsRazorPay pay= new PaymentsRazorPay(driver);
-pay.netbanking();
+		InitialAssessment assessment = new InitialAssessment(driver);
+		assessment.ViewAssessment();
 
-Thread.sleep(10000);
-Dashboard dashboard= new Dashboard(driver);
+		Service service = new Service(driver);
+		service.opinion();
 
-}
-	
+		OpinionType opinion = new OpinionType(driver);
+		opinion.USPanalButton();
+
+		Thread.sleep(10000);
+		Summary summary = new Summary(driver);
+		summary.deletecouponcode();
+		summary.summary();
+
+		PaymentsRazorPay pay = new PaymentsRazorPay(driver);
+		pay.netbanking();
+
+		Thread.sleep(10000);
+		Dashboard dashboard = new Dashboard(driver);
+
+	}
+
 }
