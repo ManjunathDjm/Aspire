fc8e994bc9c2b5cbc1dac906c46ebe342a6878b0
Manjunath D J <manjunath@onco.com>
2020-02-12 14:23:33 +0530

    increase in sleep

--- src/com/onco/webtests/NewUserOpinionUSPanalWithoutCouponInitialAssessment.java
+++ src/com/onco/webtests/NewUserOpinionUSPanalWithoutCouponInitialAssessment.java
@@ -69,7 +69,7 @@ public class NewUserOpinionUSPanalWithoutCouponInitialAssessment extends BaseCla
 		home.patientquery();
 		home.submit();
 
-		Thread.sleep(10000);
+		Thread.sleep(5000);
 		Login login = new Login(driver);
 		login.otpDetails(otp);
 
