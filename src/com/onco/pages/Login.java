6ae32837f8582afe33a30d0c11d727ab8b3cac5e
Manjunath D J <manjunath@onco.com>
2020-02-13 14:56:59 +0530

    removed implicit wait

--- src/com/onco/pages/Login.java
+++ src/com/onco/pages/Login.java
@@ -8,7 +8,9 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.CacheLookup;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.apache.log4j.Logger;
 
 /**
@@ -75,7 +77,10 @@ public class Login {
 
 		try {
 
-			Otp.click();
+			WebDriverWait wait = new WebDriverWait(ldriver, 30);
+			wait.until(ExpectedConditions.visibilityOf(Otp)).click();
+			
+			//Otp.click();
 			logger.info("OTP clicked");
 			Otp.sendKeys(otp);
 			logger.info("OTP entered");
