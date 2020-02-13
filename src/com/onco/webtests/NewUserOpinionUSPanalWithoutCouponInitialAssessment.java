aa9a1450e2cb0664c1205d72c4777b3ce2774a31
Manjunath D J <manjunath@onco.com>
2020-02-13 18:19:16 +0530

    change in wait

--- src/com/onco/webtests/NewUserOpinionUSPanalWithoutCouponInitialAssessment.java
+++ src/com/onco/webtests/NewUserOpinionUSPanalWithoutCouponInitialAssessment.java
@@ -71,11 +71,11 @@ public class NewUserOpinionUSPanalWithoutCouponInitialAssessment extends BaseCla
 		home.patientquery();
 		home.submit();
 
-		Thread.sleep(30000);
+		Thread.sleep(10000);
 		Login login = new Login(driver);
 		login.otpDetails(otp);
 
-		Thread.sleep(10000);
+		Thread.sleep(30000);
 		PatientInfo info = new PatientInfo(driver);
 		info.patientname(patientname);
 		info.cancertype();
